88191fc7b098c6c1b010b2d885bd80f81f708c63
LuisArielRH <81397914+LuisArielRetanaHernadez@users.noreply.github.com>
2024-03-19 19:35:05 -0600

    creando la clase del mobal

--- scripts/selectEffect.js
+++ scripts/selectEffect.js
@@ -17,19 +17,19 @@ effect.onclick = (e) => {
 
 const templateMobal = (...arg) => {
   return `
-    <div>
-      <div>
-        <span>copy</span>
-        <code>
+    <div class='mobal'>
+      <div class='mobal__view>
+        <span id='copy-code' class='button button--copy'>copy</span>
+        <code class='mobal__view-code>
           ${arg.code}
         </code>
       </div>
-      <div>
-        <h1>${arg.title}</h1>
-        <p>
+      <div class='mobal__description'>
+        <h1 class='title'>${arg.title}</h1>
+        <p class='mobal__text'>
           ${arg.description}
         </p>
-        <button>
+        <button class='button'>
           close
         </button>
       </div>
